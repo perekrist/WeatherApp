81895d69b0d2aac8b9a352f95913c4258f2b423f
Kristina Peregudova <perekrist31@gmail.com>
2019-12-23 10:27:45 +0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Перегудова Кристина on 23/12/2019.
+//  Copyright © 2019 perekrist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
