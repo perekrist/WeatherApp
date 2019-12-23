6ed0e9ca42662e070067e9962013816b51f0f9fd
Kristina Peregudova <perekrist31@gmail.com>
2019-12-23 11:33:58 +0700

    get weather for selected city

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var currentPosition = 0
+    
+    @IBAction func submitBtn(_ sender: Any) {
+        makeRequest(position: currentPosition)
+    }
+    
     @IBOutlet weak var picker: UIPickerView!
+    
     struct City {
         var name: String
         var lat: Double
         var lng: Double
     }
-    var cities: [City] = [City(name: "Tomsk", lat: 56.501041, lng: 84.992455), City(name: "Moscow", lat: 55.755825, lng: 37.617298),
+    
+    var cities: [City] = [City(name: "Tomsk", lat: 56.501041, lng: 84.992455),
+                          City(name: "Moscow", lat: 55.755825, lng: 37.617298),
     City(name: "Saint Petersburg", lat: 59.934280, lng: 30.335098),
     City(name: "London", lat: 51.507351, lng: -0.127758),
     City(name: "Paris", lat: 48.856613, lng: 2.352222)]
@@ -25,9 +34,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
         self.picker.delegate = self
         self.picker.dataSource = self
-        AF.request("https://api.darksky.net/forecast/49ef1022ff316233b7ba6be47faa13e6/\(cities[0].lat),\(cities[0].lng)").response { response in
-            debugPrint(response)
-        }
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -42,8 +48,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return cities[row].name
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("CurrentRow: \(row)")
+        currentPosition = row
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func makeRequest(position: Int) {
+        AF.request("https://api.darksky.net/forecast/49ef1022ff316233b7ba6be47faa13e6/\(cities[position].lat),\(cities[position].lng)").response { response in
+            debugPrint(response)
+        }
     }
 
 
