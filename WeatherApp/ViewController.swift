61fcfe5c820b3dbb2f7255a4c1b5f418e3dde024
Kristina Peregudova <perekrist31@gmail.com>
2019-12-23 11:00:52 +0700

    get request with alamofire

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    struct City {
+        var name: String
+        var lat: Double
+        var lng: Double
+    }
+    var cities: [City] = [City(name: "Tomsk", lat: 56.501041, lng: 84.992455), City(name: "Moscow", lat: 55.755825, lng: 37.617298),
+    City(name: "Saint Petersburg", lat: 59.934280, lng: 30.335098),
+    City(name: "London", lat: 51.507351, lng: -0.127758),
+    City(name: "Paris", lat: 48.856613, lng: 2.352222)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        AF.request("https://api.darksky.net/forecast/49ef1022ff316233b7ba6be47faa13e6/\(cities[0].lat),\(cities[0].lng)").response { response in
+            debugPrint(response)
+        }
     }
 
 
