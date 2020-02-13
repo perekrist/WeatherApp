0cab9de533c7a836424109703d736d2e99176dd6
perekrist <perekrist31@gmail.com>
2020-02-13 17:24:23 +0700

    finished

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     struct City {
         var name: String
@@ -18,24 +18,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         var lng: Float
     }
     
-    struct Day {
-        var day: String
-        var image: UIImage?
-        var temp: String
-    }
-    
     var cities: [City] = [ City(name: "Tomsk", lat: 56.501041, lng: 84.992455),
                           City(name: "Moscow", lat: 55.755825, lng: 37.617298),
                           City(name: "Saint Petersburg", lat: 59.934280, lng: 30.335098),
                           City(name: "London", lat: 51.507351, lng: -0.127758),
                           City(name: "Paris", lat: 48.856613, lng: 2.352222) ]
-    
-    var days: [Day] = [ Day(day: "WED", image: UIImage(named: "snow"), temp: "10"),
-                       Day(day: "SAT", image: UIImage(named: "cloudy"), temp: "16"),
-                       Day(day: "TUE", image: UIImage(named: "sunny"), temp: "6"),
-                       Day(day: "FRI", image: UIImage(named: "storm"), temp: "12"),
-                       Day(day: "MON", image: UIImage(named: "rain"), temp: "8") ]
-    
+        
     var currentPosition = 0
     
     @IBOutlet weak var currentImage: UIImageView!
@@ -47,20 +35,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
         self.picker.delegate = self
         self.picker.dataSource = self
-        makeRequest(position: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return days.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
-        
-        cell.image.image = days[indexPath.row].image
-        cell.day.text = days[indexPath.row].day
-        cell.degrees.text = days[indexPath.row].temp + "°"
-        return cell
+        makeRequest(position: currentPosition)
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -86,7 +61,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     func makeRequest(position: Int) {
          let apikey = "49ef1022ff316233b7ba6be47faa13e6"
-        
         AF.request("https://api.darksky.net/forecast/\(apikey)/\(cities[position].lat),\(cities[position].lng)").validate().responseJSON { response in
                     switch response.result {
                     case .success(let data):
@@ -97,7 +71,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                         
                         self.currentImage.image = UIImage(named: image.stringValue)
                         self.label.text = "\(currentTemp)" + " °F"
-                        
                     case .failure(let error):
                         print(error)
                     }
